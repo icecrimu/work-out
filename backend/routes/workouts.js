720e6cbe2bd6effb256ad5a106197ada3801bd22
icecrimu <charles.banigon@gmail.com>
2023-01-23 08:27:22 +0800

    Controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,42 +1,27 @@
 const express = require('express');
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout
+} = require('../controllers/workoutController');
+
 const router = express.Router();
 
-const Workout = require('../models/workoutModel');
-
 // GET all workouts
-router.get('/', (req, res) => {
-  res.json({
-    message: 'GET all workouts'
-  });
-});
+router.get('/', getWorkouts);
 
 // GET single workout
-router.get('/:id', (req, res) => {
-  res.json({
-    message: 'GET single workout'
-  });
-});
+router.get('/:id', getWorkout);
 
 // POST a new workout
-router.post('/', async (req, res) => {
-  const { title, reps, load } = req.body;
-
-  try {
-    const workout = Workout.create({ title, reps, load });
-    res.status(200).json(workout);
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.post('/', createWorkout);
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-  res.json({ message: 'DELETE a workout' });
-});
+router.delete('/:id', deleteWorkout);
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-  res.json({ message: 'UPDATE a workout' });
-});
+router.patch('/:id', updateWorkout);
 
 module.exports = router;
